0801aadd74886e3154b66cdbc4160a5c8656e5aa
Rahul Gehlot <rogerstechrk1@gmail.com>
2023-12-29 00:09:45 +0530

    updated

--- Soln_Module4/script.js
+++ Soln_Module4/script.js
@@ -1,15 +1,6 @@
 (function () {
-    var names = new Array();
-    names[0]="Ace";
-    names[1]="john";
-    names[2]="Jason";
-    names[3]="Jerry";
-    names[4]="Luffy";
-    names[5]="Zoro";
-    names[6]="Dazai";
-    names[7]="Fyodor";
-    names[8]="Jester.N ";
-    names[9]="Sigma";
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    
 
     for (var i = 0; i < names.length; i++) {
         var firstchar = names[i].charAt(0).toLowerCase();
